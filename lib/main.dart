ce6c18b4350be1504c4298d88cafa2a1b47e9d95
Abdelrahman <abdoprogrammer5@gmail.com>
2019-10-14 16:52:21 +0200

    The search image application by english language.

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'firstPage.dart';
+
+
+void main() {
+  runApp(new MaterialApp(
+    home: FirstPage(),
+    debugShowCheckedModeBanner: false,
+
+  ));
+}
+
